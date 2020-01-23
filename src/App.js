7856162f1ac2a346ce0d3cb2abf032f635cd2fff
Neil Nathan <info@neilnathan.io>
2020-01-23 17:04:04 +0100

    added cartIcon and cartDropdown components, also implemented cart reducer

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ class App extends React.Component {
           });
         });
       } else {
-        setCurrentUser(userAuth);
+        setCurrentUser({ userAuth });
       }
     });
   }
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -7,7 +7,11 @@ import "./header.styles.scss";
 
 import { auth } from "../../firebase/firebase.utils";
 
-const Header = ({ currentUser }) => (
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
+
+const Header = ({ currentUser, hidden }) => (
   <div className="header">
     <Link className="logo-container" to="/">
       <Logo className="logo"></Logo>
@@ -28,12 +32,15 @@ const Header = ({ currentUser }) => (
           SIGN IN
         </Link>
       )}
+      <CartIcon />
     </div>
+    {hidden ? null : <CartDropdown />}
   </div>
 );
 
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden
 });
 
 export default connect(mapStateToProps)(Header);
